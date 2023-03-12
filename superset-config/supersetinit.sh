707e332e5bdc5e2bf5800678d78e6e6f3e0a5d4a
Ian Sawala <iansawalasaidi@gmail.com>
2023-03-12 18:52:08 +0300

    Superset initialization fixed

---
+++ superset-config/supersetinit.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+superset fab create-admin --username admin --firstname Superset --lastname Admin --email admin@superset.com --password admin
+
+superset db upgrade
+
+superset init
